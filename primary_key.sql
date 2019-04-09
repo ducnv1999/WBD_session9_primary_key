c16733a0d9b52e599a2150faaa05d56ab56ae1f1
admin <ducgau99@gmail.com>
2019-04-09 07:39:27 +0700

    first commit

---
+++ primary_key.sql
@@ -0,0 +1,21 @@
+CREATE TABLE users(
+   user_id INT AUTO_INCREMENT PRIMARY KEY,
+   username VARCHAR(40),
+   password VARCHAR(255),
+   email VARCHAR(255)
+);
+select * from users;
+CREATE TABLE roles(
+   role_id INT AUTO_INCREMENT,
+   role_name VARCHAR(50),
+   PRIMARY KEY(role_id)
+);
+select * from roles;
+CREATE TABLE userroles(
+   user_id INT NOT NULL,
+   role_id INT NOT NULL,
+   PRIMARY KEY(user_id,role_id),
+   FOREIGN KEY(user_id) REFERENCES users(user_id),
+   FOREIGN KEY(role_id) REFERENCES roles(role_id)
+);
+select * from userroles;
